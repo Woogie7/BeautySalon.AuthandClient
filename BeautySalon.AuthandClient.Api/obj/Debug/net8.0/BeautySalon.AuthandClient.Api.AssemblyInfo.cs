46cd13802f3eed2efe898758d53194bf237e69e5
Кирилл Цынгеев <ktsyngeev@bk.ru>
2025-05-19 21:05:37 +0300

    чекаем

--- BeautySalon.AuthandClient.Api/obj/Debug/net8.0/BeautySalon.AuthandClient.Api.AssemblyInfo.cs
+++ BeautySalon.AuthandClient.Api/obj/Debug/net8.0/BeautySalon.AuthandClient.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeautySalon.AuthandClient.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb323d2b39831bc27646a28e1a2355ed4ef7ef3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeautySalon.AuthandClient.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeautySalon.AuthandClient.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
